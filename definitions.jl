0e4089dac1c15b9676047556e567d8ad6c9f0022
JakubPorubcansky <jakub.porubcansky@gmail.com>
2020-04-15 00:00:30 +0200

    athlete and segment structures

---
+++ definitions.jl
@@ -0,0 +1,13 @@
+struct Athlete
+    id::Int64
+    firstName::String
+    lastName::String
+    userName::String
+    sex::String
+end
+
+struct Segment
+    id::Int64
+    starred::Bool
+    name::String
+end
