93cf121cd735bdff881d8a1bc55fb15aa4e574a9
JakubPorubcansky <jakub.porubcansky@gmail.com>
2020-04-14 23:22:09 +0200

    config_template example

--- config_template.jl
+++ config_template.jl
@@ -1,4 +1,5 @@
-code = ""
 
-cliendId = 0
-clientSecret = ""
+const cliendId = 38512
+const clientSecret = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+code = "55b06229756e1c3eb06f6fea5a7d7c60b3c6e26b"
