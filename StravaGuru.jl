44de33e4a1b5bc16895aa94ccccc847761cfc5d7
JakubPorubcansky <jakub.porubcansky@gmail.com>
2020-04-15 00:02:05 +0200

    include definitions

--- StravaGuru.jl
+++ StravaGuru.jl
@@ -12,14 +12,17 @@
 using HTTP, JSON, Plots, Dates, TimeZones
 
 include("config.jl")
+include("definitions.jl")
 include("requests.jl")
 include("plotting.jl")
 include("utils.jl")
 
+code = "146e961350429cbb65d088fc1c2d3a8009c5bdf9"
+
 access = getAccessUsingCode(cliendId, clientSecret, code)
 access = getAccessUsingToken(cliendId, clientSecret, access["refresh_token"])
 
-getAthleteInfo(access["access_token"])
+athlete = getAthleteInfo(access["access_token"])
 
 starredSegments = getAllStarredSegments(access["access_token"])
 
